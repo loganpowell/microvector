559884cf4650bcf01b55fbcb39504065df200a77
loganpowell <loganp@tepper.cmu.edu>
2025-10-28 22:09:06 -0400

    Export PartitionStore in package __init__…

--- src/microvector/__init__.py
+++ src/microvector/__init__.py
@@ -3,10 +3,12 @@ Microvector: A simple local vector database.
 
 Main exports:
 - Client: Primary interface for vector storage and search
+- PartitionStore: Represents a cached vector store partition
 - EMBEDDING_MODEL: Default embedding model constant
 """
 
 from microvector.main import Client
+from microvector.partition import PartitionStore
 from microvector.utils import EMBEDDING_MODEL
 
-__all__ = ["Client", "EMBEDDING_MODEL"]
+__all__ = ["Client", "PartitionStore", "EMBEDDING_MODEL"]
